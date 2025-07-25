41b8d0e996d934b9a58384a739f38144291c7de8
Danu0510 <sabagithubmydanubaby@gmail.com>
2025-07-25 12:26:24 +0530

    adding contact for parameter

--- ProjectMaven/src/test/java/Contact/ContactTest.java
+++ ProjectMaven/src/test/java/Contact/ContactTest.java
@@ -6,10 +6,15 @@ import org.testng.annotations.Test;
 public class ContactTest {
 	public void contactwithPhoneTest() {
 		System.out.println("Contact with phone number");
+		String url=	System.getProperty("url");
+		String browser=	System.getProperty("browser");
+
 	String user=	System.getProperty("username");
 	String pass=	System.getProperty("password");
 	System.out.println(user);
 	System.out.println(pass);
+	System.out.println(url);
+	System.out.println(browser);
 	
 	}
 	public void deletecontact() {
